c3ca16414bc646790e3ddc4dee83b6636d1b6a62
soezayzay <67672136+soezayzay@users.noreply.github.com>
2021-02-24 11:46:36 +0630

    Add files via upload

---
+++ web_scanner.py
@@ -0,0 +1,22 @@
+#Coder:SoeZayZay
+#Date :24/2/2021
+import pyfiglet
+import nmap3
+
+banner = pyfiglet.figlet_format(" Web-Scanner")
+print(banner)
+nmap = nmap3.Nmap()
+url  = input("           Enter Website Url : ")
+data = nmap.scan_top_ports(url)
+print("")
+task="""
+           1.Copy the following json data
+        
+           2.Use Json viewer app to view the data
+    """
+print(task)
+print("==================================================================")
+print("")
+print(data)
+print("")
+print("==================================================================")
